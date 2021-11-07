ad4411fd93e96586ceb046ce3dd1946d3631f40b
gacuña <guillermo.acunaa@ug.edu.ec>
2021-11-06 20:22:34 -0500

    bootstrap

--- src/.vuepress/components/pruebas/axios.vue
+++ src/.vuepress/components/pruebas/axios.vue
@@ -1,10 +1,11 @@
+<!--
 <template>
   <div id="app">
-     <!--<ul>
+    <ul>
         <li v-for="(item, index) in info" :key="index"> 
           <b>{{item.name}}:</b> {{item.description}}
         </li>
-      </ul>-->
+      </ul>
       {{info}}
       <table class="table">
         <thead>
@@ -38,6 +39,38 @@ import axios from 'axios'
         .get('https://api.coindesk.com/v1/bpi/currentprice.json')
         .then(response => (this.info = response.data.bpi))
         .catch(e => { console.log(e)})
+    }
+  }
+</script>
+-->
+<template>
+  <div>
+    <b-table hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          {
+            age: 89,
+            first_name: 'Geneva',
+            last_name: 'Wilson',
+            _rowVariant: 'danger'
+          },
+          {
+            age: 40,
+            first_name: 'Thor',
+            last_name: 'MacDonald',
+            _cellVariants: { age: 'info', first_name: 'warning' }
+          },
+          { age: 29, first_name: 'Dick', last_name: 'Dunlap' }
+        ]
+      }
     }
   }
 </script>
